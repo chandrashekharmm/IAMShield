8bdab8b8a7d0ffde366e757712383a83215cbbba
Chandrashekharmm <mudigoudrachandru@gmail.com>
2026-06-12 12:46:31 +0530

    Fix API URL

--- backend/models/database.py
+++ backend/models/database.py
@@ -4,7 +4,7 @@ IAMShield - MongoDB Database Connection & Seed Module
 from pymongo import MongoClient
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+print("MONGO_URI =", os.getenv("MONGO_URI"))
 DB_NAME   = os.getenv("DB_NAME",   "iamshield")
 
 _client = None
